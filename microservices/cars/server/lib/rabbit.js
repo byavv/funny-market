06b910867770b3da4cff261b27ff97dc8189335a
byavv <aksenchyk.v@gmail.com>
2016-05-29 10:32:48 +0300

    [fix] cause error when start if rabbit offline

--- microservices/cars/server/lib/rabbit.js
+++ microservices/cars/server/lib/rabbit.js
@@ -1,13 +1,12 @@
 /**
  *  Simple RabbitMQ client with RPC feature. Fault tolerant.
  *  Inspired by this discussion https://github.com/squaremo/amqp.node/issues/25
- *      
+ *  Usage:  rabbit.publish('cars', { action: 'myaction', value: "somevalue" }, (err, result) => {});    
  */
 
 'use strict'
 var amqp = require('amqplib');
 var uuid = require('node-uuid');
-
 
 var Descriptor = (function () {
     var _channel;
@@ -15,6 +14,7 @@ var Descriptor = (function () {
     var _offlineCache = [];
     var _subscribtionCallback = null;
     var _name;
+    var _offline;
     // CAUTION: REPLY CONTRACT: {err: {}, result: {}} to be equal to node.js error handling convention
     var __replyHandler = (msg) => {
         if (msg.properties.correlationId
@@ -25,7 +25,6 @@ var Descriptor = (function () {
             _callbacksMap.delete(msg.properties.correlationId);
         }
     }
-
     var __consumeHandler = (msg) => {
         var result = JSON.parse(msg.content.toString());
         if (_subscribtionCallback && typeof _subscribtionCallback === 'function') {
@@ -38,9 +37,10 @@ var Descriptor = (function () {
         }
         _channel.ack(msg);
     }
-
     var __publish = (target, message, callback) => {
         try {
+            if (_offline) throw new Error("Offline");
+            if (!_channel) throw new Error("Channel closed");
             if (callback && typeof callback === 'function') {
                 var correlationId = uuid.v4();
                 _callbacksMap.set(correlationId, callback);
@@ -57,7 +57,7 @@ var Descriptor = (function () {
         }
     }
 
-    var cls = function (name) { _name = name; };
+    var cls = function (name) { _name = name; this.offline = true; };
 
     cls.prototype = {
         subscribe: (callback) => {
@@ -65,16 +65,21 @@ var Descriptor = (function () {
         },
         publish: __publish
     };
+    Object.defineProperty(cls.prototype, 'offline', {
+        get: function () { return _offline },
+        set: function (value) { _offline = value; }
+    });
     Object.defineProperty(cls.prototype, 'channel', {
         get: function () { return _channel },
-        set: function (new_value) {
-            _channel = new_value;
+        set: function (value) {
+            _channel = value;
             Promise.all([
                 _channel.assertQueue(_name, { durable: false }),
                 _channel.consume(_name, __consumeHandler, { noAck: false }),
                 _channel.consume(`amq.rabbitmq.reply-to`, __replyHandler, { noAck: true })
             ]).then(() => {
                 console.log('Rabbit client started');
+                _offline = false;
                 while (true) {
                     var pubm = _offlineCache.shift();
                     if (!pubm) {
@@ -106,30 +111,33 @@ var Client = (function () {
     };
 
     var __createConnection = function () {
-        return amqp.connect(_config.host)
-            .then((conn) => {
-                _connection = conn;
-                _connection.on('close', () => {
-                    console.log("Connection closed.");
-                });
-                _connection.on('error', (err) => {
-                    if (err.message !== "Connection closing") {
-                        console.error("[RABBIT CONNECTION ERROR]", err.message);
-                    }
-                    console.log("Try to reconnect");
-                    return setTimeout(__createConnection, _config.reconnectTime);
-                });
-                return _connection.createChannel()
-                    .then(channel => {
+        return new Promise((resolve, reject) => {
+            resolve(_descriptor);
+            amqp.connect(_config.host)
+                .then((conn) => {
+                    _connection = conn;
+                    _connection.on('close', () => {
+                        console.log("Connection closed.");
+                    });
+                    _connection.on('error', (err) => {
+                        _descriptor.offline = true;
+                        if (err.message !== "Connection closing") {
+                            console.error("[RABBIT CONNECTION ERROR]", err.message);
+                        }
+                        console.log("Try to reconnect");
+                        return setTimeout(__createConnection, _config.reconnectTime);
+                    });
+                    _connection.createChannel().then((channel) => {
                         _descriptor.channel = channel;
-                        return _descriptor;
-                    })
-            }, (err) => {
-                console.log(err);
-                console.log("Try to reconnect");
-                setTimeout(__createConnection, _config.reconnectTime);
-            })
-    }
+                    });
+                }, (err) => {
+                    _descriptor.offline = true;
+                    console.log(err);
+                    console.log("Try to reconnect");
+                    setTimeout(__createConnection, _config.reconnectTime);
+                })
+        });
+    };
 
     cls.prototype = {
         open: () => {
