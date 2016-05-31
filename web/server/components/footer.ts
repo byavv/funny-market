5b6060896e9fb1d4099d51f9e1e94050462b0996
byavv <aksenchyk.v@gmail.com>
2016-05-31 21:36:21 +0300

    [chore] review rendering config, move footer rendering on server

---
+++ web/server/components/footer.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+import {ROUTER_DIRECTIVES, Router} from '@angular/router-deprecated';
+
+@Component({
+  selector: 'app-footer',
+  template: require('./footer.html'),
+  directives: [ROUTER_DIRECTIVES]
+})
+export class Footer implements OnInit {
+  constructor() { }
+  seo = {
+    title: 'SEO'
+  };
+  ngOnInit() { }
+}
--- web/server/server.ts
+++ web/server/server.ts
@@ -1,101 +1,103 @@
 import * as path from 'path';
 import * as express from 'express';
-var history = require('connect-history-api-fallback');
+const compression = require('compression');
 
-
-const host = process.env.HTTP_HOST || "0.0.0.0",
-  http_port = process.env.HTTP_PORT || 3000,
-  etcd_host = process.env.ETCD_HOST || '192.168.99.100', // etcd in prod
-  origin = process.env.ORIGIN_URL || 'http://localhost:3001';
+const http_port = process.env.HTTP_PORT || 3000
+    , etcd_host = process.env.ETCD_HOST || '192.168.99.100'
+    , proxy_url = process.env.PROXY_URL || 'http://localhost:3001';
 
 const registry = require('etcd-registry')(`${etcd_host}:4001`);
 
 import 'angular2-universal/polyfills';
+import { Http } from '@angular/http';
+import { Footer } from './components/footer';
 import {
-  provide,
-  enableProdMode,
-  expressEngine,
-  REQUEST_URL,
-  ORIGIN_URL,
-  BASE_URL,
-  NODE_ROUTER_PROVIDERS,
-  NODE_PLATFORM_PIPES,
-  NODE_HTTP_PROVIDERS,
-  ExpressEngineConfig, BootloaderConfig, queryParamsToBoolean
+    provide,
+    enableProdMode,
+    expressEngine,
+    REQUEST_URL,
+    ORIGIN_URL,
+    BASE_URL,
+    NODE_ROUTER_PROVIDERS,
+    NODE_LOCATION_PROVIDERS,
+    NODE_HTTP_PROVIDERS,
+    NODE_PRELOAD_CACHE_HTTP_PROVIDERS
 } from 'angular2-universal';
+import {
+    TranslateService,
+    TranslateLoader,
+    TranslateStaticLoader
+} from "ng2-translate/ng2-translate";
 
+// Root app Component
 import {App} from '../client/app/app';
-import {Html} from './components/html';
 
-const app = express();
-var router = express.Router();
-const ROOT = path.join(path.resolve(__dirname, '../build'));
-
-app.use(require("serve-favicon")(path.join(__dirname, "./views/favicon.ico")));
+// Disable Angular 2's "development mode".
+// See: https://angular.io/docs/ts/latest/api/core/enableProdMode-function.html
 enableProdMode();
+// express app
+let app = express();
+
+// Root directory
+let root = path.join(path.resolve(__dirname, '..'));
+
+// Compress assets
+app.use(compression());
+
+// Definition static resources folder '/build/cl' and map to '/static' path
+//app.use('/static', express.static(path.join(root, 'build', 'cl'), { index: false }));
+app.use('/build', express.static(path.join(root, 'build'), { index: false }));
+app.use(require("serve-favicon")(path.join(__dirname, "./views/favicon.ico")));
 app.engine('.html', expressEngine);
 app.set("views", path.join(__dirname, "./views"));
 app.set('view engine', 'html');
 
 function ngApp(req, res) {
-  let queryParams: any = queryParamsToBoolean(req.query);
-  console.log(queryParams, req.originalUrl)
-  let options: BootloaderConfig = Object.assign(queryParams, {
-    directives: [Html],
-    platformProviders: [
-      provide(ORIGIN_URL, { useValue: origin }),
-      provide(BASE_URL, { useValue: '/' }),
-    ],
-    providers: [
-      provide(REQUEST_URL, { useValue: req.originalUrl }),
-      NODE_PLATFORM_PIPES,
-      NODE_ROUTER_PROVIDERS,
-      NODE_HTTP_PROVIDERS
-    ],
-    data: {},
-    precache: true, //<-- unknown
-    primeCache: true,//<-- unknown
-    async: false, // if set to true, app will be desync
-    preboot: queryParams.preboot === false ? null : {
-      appRoot: 'app', // we need to manually include the root
-      start: true,
-      // freeze: 'spinner',     // show spinner w button click & freeze page
-      replay: 'rerender',    // rerender replay strategy
-      buffer: true,          // client app will write to hidden div until bootstrap complete
-      debug: false,
-      uglify: true,
-      buttonPress: true,
-      presets: ['keyPress', 'buttonPress', 'focus']
-    },
-    ngOnRendered: () => {
-      console.log('DONE');
-    },
-    ngDoCheck: () => {
-      console.log("check")
-      return true;
-    }
-  });
-  return res.render('index', options);
-}
-
-app.use('/build/', express.static(ROOT, { index: false }));
-router.route("*").get(ngApp);
-router.route("/search*").get(ngApp);
-//router.route("/login*").get(ngApp);
-//router.route("/account*").get(ngApp);
-app.use(history({}))
-app.use(router);
-
-app.listen(http_port, host, function () {
-  console.log(`WEB server is listening on port: ${http_port} `)
-  registry.join('web', { port: http_port });
-  setTimeout(() => {
-    registry.lookup('web', (err, service) => {
-      if (service) {
-        console.log(`Web server is registered on ${service.url} `);
-      } else {
-        console.log(`Web server egistration failed`);
-      }
+    let baseUrl = '/';
+    let url = req.originalUrl || '/';
+    res.render('index', {
+        directives: [App, Footer],
+        platformProviders: [
+            provide(ORIGIN_URL, { useValue: proxy_url }),
+            provide(BASE_URL, { useValue: baseUrl }),
+        ],
+        providers: [
+            NODE_ROUTER_PROVIDERS,
+            NODE_LOCATION_PROVIDERS,
+            NODE_PRELOAD_CACHE_HTTP_PROVIDERS,
+            provide(REQUEST_URL, { useValue: url }),
+            provide(TranslateLoader, {
+                useFactory: (http: Http) => new TranslateStaticLoader(http, 'i18n', '.json'),
+                deps: [Http]
+            })
+        ],
+        preboot: {
+            appRoot: 'app',          // selector for Angular root element
+            replay: 'rerender',      // Angular will re-render the view
+            freeze: 'spinner',       // show spinner w button click & freeze page
+            focus: true,             // maintain focus after re-rendering
+            buffer: true,            // client app will write to hidden div until bootstrap complete
+            keyPress: true,          // all keystrokes in text elements recorded
+            buttonPress: true        // when button pressed, record and freeze page          
+        },
+        async: true
     });
-  }, 1000);
+};
+
+// Specifies that all server-side paths should be routed to our ngApp function (see above)
+app.get('/*', ngApp);
+
+// Binds our express app the the specified port (i.e. starts it up) and logs when it is running
+app.listen(http_port, () => {
+    console.log(`WEB server is listening on port: ${http_port} `)
+    registry.join('web', { port: http_port });
+    setTimeout(() => {
+        registry.lookup('web', (err, service) => {
+            if (service) {
+                console.log(`Web server is registered on ${service.url} `);
+            } else {
+                console.log(`Web server egistration failed`);
+            }
+        });
+    }, 1000);
 });
