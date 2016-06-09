1a99d95d88afe74db7c2eddb69514d671dcaed7d
byavv <aksenchyk.v@gmail.com>
2016-06-09 20:24:56 +0300

    [chore] move gateway to sub repo

--- gateway/server/lib/errors/error_401.js
+++
@@ -1,8 +0,0 @@
-function UnauthorizedAccessError(message) {
-    Error.call(this);
-    Error.captureStackTrace(this, arguments.callee);
-    this.name = "UnauthorizedAccessError";
-    this.message = message;
-    this.status = 401;
-}
-module.exports = UnauthorizedAccessError;
